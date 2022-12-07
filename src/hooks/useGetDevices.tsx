7b71220fe77de2cb8a95350f888b8ecf89745a3c
Nam Tran <nam.tran.ext@personio.de>
2022-12-07 22:21:15 +0700

    (feat) Show selected device

--- src/hooks/useGetDevices.tsx
+++ src/hooks/useGetDevices.tsx
@@ -4,18 +4,29 @@ export interface RawDevice {
   _name: string;
   coreaudio_device_output?: number;
   coreaudio_device_input?: number;
+  coreaudio_device_transport: string;
+  coreaudio_default_audio_output_device?: string;
 }
+
 export interface Device {
   name: string;
+  isSelected: boolean;
 }
 
 const useGetDevices = (): Device[] => {
   const rawData = runAppleScriptSync('return do shell script "/usr/sbin/system_profiler SPAudioDataType -json"');
   const parsedRawDevices: Array<RawDevice> = JSON.parse(rawData)["SPAudioDataType"][0]["_items"];
 
-  return parsedRawDevices.map((rawDevice) => ({
-    name: rawDevice._name,
-  }));
+  return parsedRawDevices
+    .filter((rawDevice) => {
+      return (
+        !!rawDevice.coreaudio_device_output && rawDevice.coreaudio_device_transport === "coreaudio_device_type_builtin"
+      );
+    })
+    .map((rawDevice) => ({
+      name: rawDevice._name,
+      isSelected: !!rawDevice.coreaudio_default_audio_output_device,
+    }));
 };
 
 export default useGetDevices;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,29 @@
-import { List, Action, ActionPanel } from "@raycast/api";
+import { List, Action, ActionPanel, Icon, Color } from "@raycast/api";
 import { runAppleScriptSync } from "run-applescript";
-import useGetDevices from "./hooks/useGetDevices";
+import useGetDevices, { Device } from "./hooks/useGetDevices";
+import { useState, useEffect } from "react";
 
 const Index = () => {
-  const outputDevices = useGetDevices();
+  const [outputDevices, setOutputDevices] = useState<Device[]>([]);
+  const refreshDataLoop = () => {
+    setOutputDevices(useGetDevices());
+    setTimeout(() => refreshDataLoop(), 300);
+  };
+
+  useEffect(() => {
+    setOutputDevices(useGetDevices());
+    refreshDataLoop();
+  }, []);
+
   return (
     <List>
       {outputDevices.map((outputDevice) => (
         <List.Item
+          icon={
+            outputDevice.isSelected
+              ? { source: Icon.CheckCircle, tintColor: Color.Green }
+              : { source: Icon.CheckCircle, tintColor: Color.SecondaryText }
+          }
           key={outputDevice.name}
           title={outputDevice.name}
           actions={
